c6ca760c36b52a84a57f57d8c5f5451d2e90cfdc
ductoanvo <hey.toan@outlook.com>
2022-04-04 18:15:24 +0700

    initial commit

---
+++ src/main/java/vn/edu/iuh/fit/se/thubonggiareapis/ThuBongGiaReRestApisSpringBootApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se.thubonggiareapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuBongGiaReRestApisSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThuBongGiaReRestApisSpringBootApplication.class, args);
+    }
+
+}
