3f85af9e1df5906bbdf473f4a0aa2beb820cff2d
ductoanvo <toan.ducvo@icloud.com>
2022-05-15 16:16:47 +0700

    feat: encode user password with spring security

--- src/main/java/vn/edu/iuh/fit/se/thubonggiareapis/ThuBongGiaReRestApisSpringBootApplication.java
+++ src/main/java/vn/edu/iuh/fit/se/thubonggiareapis/ThuBongGiaReRestApisSpringBootApplication.java
@@ -2,8 +2,11 @@ package vn.edu.iuh.fit.se.thubonggiareapis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class
+})
 public class ThuBongGiaReRestApisSpringBootApplication {
 
     public static void main(String[] args) {
