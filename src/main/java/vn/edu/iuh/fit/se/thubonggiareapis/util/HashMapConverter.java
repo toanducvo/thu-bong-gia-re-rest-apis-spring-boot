f44475eb60ece295d33519887cd8e9235376a6af
ductoanvo <toan.ducvo@icloud.com>
2022-05-18 11:11:57 +0700

    change hashmap converter args

--- src/main/java/vn/edu/iuh/fit/se/thubonggiareapis/util/HashMapConverter.java
+++ src/main/java/vn/edu/iuh/fit/se/thubonggiareapis/util/HashMapConverter.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 
 public abstract class HashMapConverter {
-    public static HashMap<String, Object> toHashMap(Object object, Class<?> clazz) {
+    public static HashMap<String, Object> toHashMap(Object object) {
         HashMap<String, Object> hashMap = new HashMap<>();
-        for (Field field : clazz.getDeclaredFields()) {
+        for (Field field : object.getClass().getDeclaredFields()) {
             field.setAccessible(true);
             try {
                 hashMap.put(field.getName(), field.get(object));
@@ -22,7 +22,7 @@ public abstract class HashMapConverter {
     public static List<HashMap<String, Object>> toListOf(List<?> list) {
         List<HashMap<String, Object>> listOfHashMap = new ArrayList<>();
         list.forEach(o -> {
-            HashMap<String, Object> item = toHashMap(o, o.getClass());
+            HashMap<String, Object> item = toHashMap(o);
             listOfHashMap.add(item);
         });
         return listOfHashMap;
